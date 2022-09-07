78514bc32239181869d9ada1d656cc73549be526
Alexander <a.klivets95@gmail.com>
2022-09-07 11:10:37 +0300

    Исправление ошибок

--- source/js/script.js
+++ source/js/script.js
@@ -1,21 +1,21 @@
 let navMain = document.querySelector('.main-nav__wrapper');
-let navToggle = document.querySelector('.main-nav__toogle');
+let navToggle = document.querySelector('.main-header__toogle');
 
 navMain.classList.remove('main-nav__wrapper--nojs');
-navToggle.classList.remove('main-nav__toogle--nojs');
+navToggle.classList.remove('main-header__toogle--nojs');
 
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-nav__wrapper--closed')) {
     navMain.classList.remove('main-nav__wrapper--closed');
     navMain.classList.add('main-nav__wrapper--open');
-    navToggle.classList.remove('main-nav__toogle--closed');
-    navToggle.classList.add('main-nav__toogle-open');
+    navToggle.classList.remove('main-header__toogle--closed');
+    navToggle.classList.add('main-header__toogle-open');
   } else {
     navMain.classList.add('main-nav__wrapper--closed');
     navMain.classList.remove('main-nav__wrapper--open');
-    navToggle.classList.add('main-nav__toogle--closed');
-    navToggle.classList.remove('main-nav__toogle-open');
+    navToggle.classList.add('main-header__toogle--closed');
+    navToggle.classList.remove('main-header__toogle-open');
   }
 });
 
